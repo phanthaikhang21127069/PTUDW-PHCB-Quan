730e294b62d113c3a49297ef8ea5e73cb68b7a89
phanthaikhang21127069 <101028729+phanthaikhang21127069@users.noreply.github.com>
2023-12-06 06:46:06 -0800

    create

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = 3800;
+const expressHbs = require('express-handlebars');
+
+app.use(express.static(__dirname + '/html'));
+
+app.engine('hbs', expressHbs.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    defaultLayout: 'layout',
+    extname: 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    }, 
+    helpers: {
+        showDate: (date) => {
+            return date.toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            });
+        },
+        showIndex: (index) => index + 1,
+    }
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => res.redirect('/danh-sach'));
+app.use('/danh-sach', require('./routes/manageListRouter'));
+app.use('/yeu-cau', require('./routes/requestRouter'));
+app.use('/bao-cao', require('./routes/reportRouter'));
+app.use('/thong-bao', require('./routes/notificationRouter'));
+app.use('/diem-dat-bang-quang-cao', require('./routes/adsAddressRouter'));
+
+
+// app.get('/createTables', (req, res) => {
+//     let models = require('./models');
+//     models.sequelize.sync().then(() => {
+//         res.send('Tables created!');
+//     });
+// });
+// app.set('view engine', 'hbs');
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ routes/reportRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/reportController");
+
+router.get("/", controller.show);
+// router.post("/wards", controller.addWard);
+// router.put("/", controller.editWard);
+// router.delete("/:id", controller.deleteWard);
+
+module.exports = router;
