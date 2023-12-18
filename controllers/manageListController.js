394e9424f2879785d4ead18a69af0e0ed7f045f7
phanthaikhang21127069 <101028729+phanthaikhang21127069@users.noreply.github.com>
2023-12-18 23:15:08 +0700

    add request ads to manage list

--- controllers/manageListController.js
+++ controllers/manageListController.js
@@ -78,6 +78,37 @@ controller.show = async (req, res) => {
     order: [["createdAt", "DESC"]],
   });
 
+  res.locals.requestadsquans = await models.Requestadsquan.findAll({
+    include: [{
+      model: models.Place,
+      attributes: [
+        "diaChi",
+        "khuVuc",
+        "loaiVT",
+        "longitude",
+        "latitude"
+      ],
+    }],
+    attributes: [
+      "id",
+      "congTy",
+      "diaChiCongTy",
+      "dienThoai",
+      "email",
+      "tenBangQuangCao",
+      "loaiQC",
+      "kichThuoc",
+      "soLuong",
+      "ngayBatDau",
+      "ngayKetThuc",
+      "tinhTrang"
+    ],
+    order: [["congTy", "ASC"]],
+    // where:{
+    //   khuVuc:"Phường 4, Quận 5"
+    // }
+  });
+
   res.render("manage-list", {
     placedetails: res.locals.placedetails.map(detail => ({
       ...detail.toJSON(),
@@ -137,4 +168,113 @@ controller.requestEditAds = async (req, res) => {
 }
 }
 
+controller.deleteRequest=async(req,res)=>{
+  console.log("ok");
+  let id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
+  try {
+    await models.Requestadsquan.destroy(
+      {where: {id}}
+    );
+    res.send("Đã xoá yêu cầu!");
+  } catch (error) {
+    res.send("Không thể xoá yêu cầu!");
+    console.error(error);
+  }
+}
+
+
+controller.addRequest = async (req, res) => {
+  if (!req.body || typeof req.body !== 'object') {
+    res.send('Invalid request body');
+    return;
+  }
+
+  const {
+    congTy,
+    diaChiCongTy,
+    dienThoai,
+    email,
+    diaChiRequest,
+    tenBangQuangCao,
+    loaiQC,
+    kichThuoc,
+    soLuong,
+    ngayBatDau,
+    ngayKetThuc
+  } = req.body;
+  console.log(req.body);
+
+  const requestPlace = await models.Place.findOne({
+    attributes: ["id"],
+    where: { diaChi: diaChiRequest }
+  });
+  let placeId = requestPlace.getDataValue("id");
+
+
+  try {
+    await models.Requestadsquan.create({
+      congTy,
+      diaChiCongTy,
+      dienThoai,
+      email,
+      placeId: placeId,
+      tenBangQuangCao,
+      loaiQC,
+      kichThuoc,
+      soLuong,
+      ngayBatDau,
+      ngayKetThuc,
+      tinhTrang: 'Chờ phê duyệt'
+    });
+    res.redirect('/yeu-cau');
+  } catch (error) {
+    res.send('Không thể thêm');
+    console.error(error);
+  }
+};
+
+controller.editRequest = async (req, res) => {
+  let {id,
+    congTy,
+    diaChiCongTy,
+    dienThoai,
+    email,
+    diaChiRequest,
+    tenBangQuangCao,
+    loaiQC,
+    kichThuoc,
+    soLuong,
+    ngayBatDau,
+    ngayKetThuc,
+    tinhTrang} = req.body;
+  
+    const requestPlace = await models.Place.findOne({ 
+      attributes: ["id"],
+      where: {diaChi: diaChiRequest} 
+    });
+    let placeId = requestPlace.getDataValue("id");
+  try {
+    await models.Requestadsquan.update(
+      { 
+        congTy,
+        diaChiCongTy,
+        dienThoai,
+        email,
+        placeId:placeId,
+        tenBangQuangCao,
+        loaiQC,
+        kichThuoc,
+        soLuong,
+        ngayBatDau,
+        ngayKetThuc,
+        tinhTrang
+      },
+      {where: {id}}
+    );
+    res.send("Đã cập nhật yêu cầu!");
+  } catch (error) {
+    res.send("Không thể cập nhật yêu cầu!");
+    console.error(error);
+  }
+};
 module.exports = controller;
